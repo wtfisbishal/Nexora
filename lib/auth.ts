af3f8a58c5431deb5e4cf5f67e8f4c4617df3777
bisxxal <bishalkandi859494@gmail.com>
2026-09-22 00:14:34 +0530

    fix:auth-errror

--- lib/auth.ts
+++ lib/auth.ts
@@ -21,6 +21,7 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   session: {
